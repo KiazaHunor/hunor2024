c2d271a3df51a42da5919c9eb83279acc326f7f3
Horváth Hunor <horvathh21@ady-nagyatad.sulinet.hu>
2024-09-06 13:31:55 +0200

    a

---
+++ Asztali/Walesi Bárdok/Walesi Bárdok/Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+Console.WriteLine("Hello, World!");
+
+StreamReader vers = new StreamReader("janos.txt");
+
+
+string line;
+
+while ((line = vers.ReadLine()) != null)
+{
+    Console.WriteLine(line);
+}
+
+
+Dictionary<string, string> openWith =
+    new Dictionary<string, string>();
+    openWith.Add("txt", "notepad.exe");
+
+
