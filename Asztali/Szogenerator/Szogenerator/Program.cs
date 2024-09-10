2795d6816bf208c69445d98aa36c95204931332d
Horváth Hunor <horvathh21@ady-nagyatad.sulinet.hu>
2024-09-10 13:24:06 +0200

    a

---
+++ Asztali/Szogenerator/Szogenerator/Program.cs
@@ -0,0 +1,20 @@
+﻿
+StreamReader be = new StreamReader("alanyok.txt");
+string alany = be.ReadToEnd();
+
+Console.WriteLine(alany);
+
+
+StreamReader ki = new StreamReader("ige.txt");
+string ige = ki.ReadToEnd();
+
+Console.WriteLine(ige);
+
+
+
+StreamReader koze = new StreamReader("melleknev.txt");
+string melleknev = koze.ReadToEnd();
+
+Console.WriteLine(melleknev);
+
+
