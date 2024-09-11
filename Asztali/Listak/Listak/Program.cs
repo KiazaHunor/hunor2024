99d23c0877cfe8f51a819e89e3f1568279686e9b
Horváth Hunor <horvathh21@ady-nagyatad.sulinet.hu>
2024-09-11 12:35:40 +0200

    a

---
+++ Asztali/Listak/Listak/Program.cs
@@ -0,0 +1,38 @@
+﻿static List<string> fuggveny()
+{
+
+
+
+    List<string> lista = new List<string>();
+    StreamReader beker = new StreamReader("forras.txt");
+
+    while (!beker.EndOfStream)
+    {
+        string sor = beker.ReadLine();
+        lista.Add(sor);
+    }
+    Console.WriteLine(lista.Count);
+    beker.Close();
+    return lista;
+}
+static List<string> szavak(List<string> lista3)
+{
+    return lista3;
+}
+
+List <string> lista2 = fuggveny();
+
+Console.WriteLine(lista2.Count);   
+
+
+
+/*
+for(int i = 0; i < lista.Count; i++) 
+{
+    if(lista.Count) 
+    {
+        Console.WriteLine(lista[i]);
+    }
+    
+    
+}*/
