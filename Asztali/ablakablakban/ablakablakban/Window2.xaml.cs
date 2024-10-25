a602a29a41e4056dce68cfba729ee68e2d7698b2
Horváth Hunor <horvathh21@ady-nagyatad.sulinet.hu>
2024-10-25 14:20:33 +0200

    a

---
+++ Asztali/ablakablakban/ablakablakban/Window2.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ablakablakban
+{
+    /// <summary>
+    /// Interaction logic for Window2.xaml
+    /// </summary>
+    public partial class Window2 : Window
+    {
+        MainWindow szul;
+
+        public Window2(MainWindow szul)
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            szul.button.Content = "OK";
+            this.Close();
+        }
+    }
+}
---
+++ Asztali/ujablak/ujablak/uj.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ujablak
+{
+    /// <summary>
+    /// Interaction logic for uj.xaml
+    /// </summary>
+    public partial class ujAblak : Window
+    {
+        Button gomb;
+        public ujAblak(Button gomb)
+        {
+            this.gomb=gomb;
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            gomb.Content=textBox.Text;
+            this.Close();
+        }
+    }
+}
