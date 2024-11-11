4e3f8ff8648b7328258270d0cec23d5916c089dc
Horváth Hunor <horvathh21@ady-nagyatad.sulinet.hu>
2024-11-11 14:20:35 +0100

    a

---
+++ Asztali/operatorok/operatorok/Pont.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace operatorok
+{
+    internal class Pont
+    {
+        public int X;
+        public int Y;
+
+        public Pont(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+        public Pont(int x) 
+        {
+            X = x;
+            Y = 0;
+        }
+        public Pont() 
+        {
+            X=0;
+            Y=0;
+        }
+
+        public static Pont operator + (Pont a, Pont b) 
+        {
+            return new Pont (a.X+b.X, a.Y+b.Y);
+        }
+
+        public override string ToString()
+        {
+            return $"X:{X}, X:{Y}";
+        }
+    }
+}
---
+++ Asztali/operatorok/operatorok/Program.cs
@@ -0,0 +1,5 @@
+﻿using operatorok;
+
+Pont pi1 = new Pont(1, 1);
+Pont pi2 = new Pont(2, 3);
+Console.WriteLine(p1 + p2);
