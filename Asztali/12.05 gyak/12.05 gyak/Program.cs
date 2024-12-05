b9a9d92a942e473ebd9ca786040f16714de68136
Horváth Hunor <horvathh21@ady-nagyatad.sulinet.hu>
2024-12-05 08:43:55 +0100

    a

---
+++ Asztali/12.05 gyak/12.05 gyak/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Text.RegularExpressions;
+
+class Program
+{
+    const string emailPattern = @"[^@]+\@[^\.]+\..+";
+
+    static IEnumerable<string> TestCases
+    {
+        get
+        {
+            yield return "user@test.com";
+            yield return "test.com";
+            yield return "user@.com";
+            yield return "user@1234.com";
+            yield return "12elemer@server.be";
+            yield return "valaki@mymail.hu";
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        var expression = new Regex(emailPattern, RegexOptions.Compiled);
+
+        foreach (var testcase in TestCases)
+        {
+            bool match = expression.IsMatch(testcase);
+            Console.WriteLine("{0} match status: {1}", testcase, match);
+        }
+
+        Console.ReadKey();
+    }
+}
