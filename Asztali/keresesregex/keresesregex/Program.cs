c5f756f590494a136baf6fce28454d3b7d066711
Horváth Hunor <horvathh21@ady-nagyatad.sulinet.hu>
2024-12-16 14:20:30 +0100

    a

---
+++ Asztali/keresesregex/keresesregex/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.RegularExpressions;
+
+
+string text = File.ReadAllText("szoveg.txt");
+
+Regex minta = new Regex(@"https://www\.\S+\.\S+");
+
+var talalat = minta.Matches(text);
+Console.WriteLine(talalat.Count);
+foreach (Match elem in talalat)
+{
+    Console.WriteLine(elem);
+}
