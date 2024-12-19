ccda669639094ea6f236181d171d4a3dacd533e9
Horváth Hunor <horvathh21@ady-nagyatad.sulinet.hu>
2024-12-19 08:44:47 +0100

    a

---
+++ Asztali/erettsegi/erettsegi/Program.cs
@@ -0,0 +1,17 @@
+﻿//1. Feladat
+string trafi = "jeladas.txt";
+string[] trafipax = File.ReadAllLines(trafi);
+Console.WriteLine(trafipax);
+
+foreach (string kapcsolas in trafipax)
+{
+    Console.WriteLine(kapcsolas);
+}
+
+
+//2.Feladat
+Console.WriteLine("2.Feladat");
+for (int i = 0; i < trafipax.Length; i++) 
+{
+    if (trafipax[2])
+}
