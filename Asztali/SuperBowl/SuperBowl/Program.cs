1806302d176e35f15718b935d24b4ac8a40f13c2
Horváth Hunor <horvathh21@ady-nagyatad.sulinet.hu>
2025-01-06 14:18:10 +0100

    b

---
+++ Asztali/SuperBowl/SuperBowl/Program.cs
@@ -0,0 +1,19 @@
+﻿StreamReader olvas = new StreamReader("SuperBowl.txt");
+
+List<string> ssz = new List<string>();
+List<string> datum = new List<string>();
+List<string> gyoztes = new List<string>();
+List<string> eredmeny = new List<string>();
+List<string> vesztes = new List<string>();
+List<string> helyszin = new List<string>();
+List<string> varosallam = new List<string>();
+List<string> nezoszam = new List<string>();
+
+
+
+while (!olvas.EndOfStream) 
+{
+    string line = olvas.ReadLine();
+    string[] vag = line.Split(",");
+}
+olvas.Close();
