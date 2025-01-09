518015fa25a8c29365acfe963eae41d9abf6f07c
Horváth Hunor <horvathh21@ady-nagyatad.sulinet.hu>
2025-01-09 10:23:04 +0100

    a

---
+++ marci/szoftver/szoftver/Program.cs
@@ -0,0 +1,16 @@
+﻿int napok_szama = 12;
+int[] homersekletek = [-12, -9, -5, -2, 0, 2, 4, 6];
+
+int fagyosnap = 0;
+
+int nap = 0;
+
+for (int i = 0; i < napok_szama; i++) 
+{
+    if (homersekletek[i] < 0) 
+    {
+        fagyosnap++;
+        
+    }
+}
+Console.WriteLine(fagyosnap);
