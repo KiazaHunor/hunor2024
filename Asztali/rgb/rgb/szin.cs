e586fe713cc6c707b4fa83258273ff855f677675
Horváth Hunor <horvathh21@ady-nagyatad.sulinet.hu>
2025-02-10 14:19:37 +0100

    a

---
+++ Asztali/rgb/rgb/szin.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rgb
+{
+    internal class szin
+    {
+        public byte r;
+        public byte g;
+        public byte b;
+
+        public szin(int red,int green, int blue) 
+        {
+            r = (byte)red;
+            g = (byte)green;
+            b = (byte)blue;
+        }
+    }
+}
