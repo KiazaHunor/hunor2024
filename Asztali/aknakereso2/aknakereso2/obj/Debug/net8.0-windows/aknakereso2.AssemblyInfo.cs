e89490ed0994aac0e2ab79a12661596f9d612662
Horváth Hunor <horvathh21@ady-nagyatad.sulinet.hu>
2025-03-07 13:23:14 +0100

    a

--- Asztali/aknakereso2/aknakereso2/obj/Debug/net8.0-windows/aknakereso2.AssemblyInfo.cs
+++ Asztali/aknakereso2/aknakereso2/obj/Debug/net8.0-windows/aknakereso2.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("aknakereso")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("aknakereso2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba3cd63dd7a075c06e28594b0eb11ec142bba07c")]
-[assembly: System.Reflection.AssemblyProductAttribute("aknakereso")]
-[assembly: System.Reflection.AssemblyTitleAttribute("aknakereso")]
+[assembly: System.Reflection.AssemblyProductAttribute("aknakereso2")]
+[assembly: System.Reflection.AssemblyTitleAttribute("aknakereso2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 [assembly: System.Runtime.Versioning.TargetPlatformAttribute("Windows7.0")]
 [assembly: System.Runtime.Versioning.SupportedOSPlatformAttribute("Windows7.0")]
