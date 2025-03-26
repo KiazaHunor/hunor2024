4c27105c778df12471100c46d0bea60b5f0072c5
Horváth Hunor <horvathh21@ady-nagyatad.sulinet.hu>
2025-03-26 12:24:36 +0100

    a

---
+++ Asztali/szinuszgorbe/szinuszgorbe/MainWindow.xaml.cs
@@ -0,0 +1,102 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace szinuszgorbe
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+    //1.kordinata rendszer
+    //2.kör(vekony)
+    //3.fekete pont
+    //4.sugar
+    //5.magassag(piros)
+    //6.szinusz görbe
+    //7.korív(nagy)
+    //8.körív(kicsi)
+    //9.adattábla(szög egyebek)
+    //sine curve and the unit circle
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void vaszon_Loaded(object sender, RoutedEventArgs e)
+        {
+            kordinataRajzol();
+            origoY = Convert.ToInt32(Height / 2);
+            origoX = Convert.ToInt32(r * 1.1);
+        }
+        int r = 100;
+        double origoY = 0;
+        double origoX = 0;
+        void kordinataRajzol()
+        {
+            
+
+            Line xTengely = new Line();
+            xTengely.Stroke = Brushes.Black;
+            xTengely.X1 = 0;
+            xTengely.Y1 = Height / 2;
+            xTengely.X2 = Width;
+            xTengely.Y2 = Height / 2;
+            origoY = Height / 2;
+
+
+            Line yTengely = new Line();
+            yTengely.Stroke = Brushes.Black;
+            yTengely.X1 = r * 1.1;
+            yTengely.Y1 = 0;
+            yTengely.X2 = r * 1.1;
+            yTengely.Y2 = Height;
+
+
+            vaszon.Children.Add(xTengely);
+            vaszon.Children.Add(yTengely);
+
+            for (int i = 0; i < 5; i++)
+            {
+                Line vonalka = new Line();
+                vonalka.Stroke = Brushes.Black;
+                vonalka.X1 = i * 90 + yTengely.X1;
+                vonalka.Y1 = Height / 2 - 5;
+                vonalka.X2 = i * 90 + yTengely.X1;
+                vonalka.Y2 = Height / 2 + 5;
+                vaszon.Children.Add(vonalka);
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                Line vonalka = new Line();
+                vonalka.Stroke = Brushes.Black;
+                vonalka.X1 = yTengely.X1 - 5;
+                vonalka.Y1 = i * 90 + (xTengely.Y1 - 180);
+                vonalka.X2 = yTengely.X1 + 5;
+                vonalka.Y2 = i * 90 + (xTengely.Y1 - 180);
+                vaszon.Children.Add(vonalka);
+            }
+        }
+        void kor(int x)
+        {
+            Ellipse kor = new Ellipse();
+            kor.Stroke = Brushes.Blue;
+            kor.Height = 2 * r;
+            kor.Width = 2 * r;
+            kor.Margin = new Thickness(x- r, origoY -r, 0,0);
+
+            vaszon.Children.Add(kor);
+            
+        }
+    }
+}
