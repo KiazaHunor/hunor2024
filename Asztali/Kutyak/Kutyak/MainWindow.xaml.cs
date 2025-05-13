68463544ff011c0895bce115ee6ea57966d0e9b7
Horváth Hunor <horvathh21@ady-nagyatad.sulinet.hu>
2025-05-13 13:22:14 +0200

    a

---
+++ Asztali/Kutyak/Kutyak/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Kutyak
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        List<KutyaNev> kutyaNevek = new List<KutyaNev>();
+        List<KutyaFajta> kutyaFajtak = new List<KutyaFajta>();
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            string[] sorok = File.ReadAllLines("KutyaNevek.csv");
+            for (int i = 1; i < sorok.Length; i++) 
+            {
+                string[] vag = sorok[i].Split(";");
+                kutyaNevek.Add(new KutyaNev(Convert.ToInt32(vag[0]), vag[1]));
+            }
+
+            sorok = File.ReadAllLines("KutyaFajtak.csv");
+            for(int i = 1;i < sorok.Length; i++) 
+            {
+                string[] vag =sorok[i].Split(";");
+                kutyaFajtak.Add(new KutyaFajta(int.Parse(vag[0])));
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            label2.Content = kutyaNevek.Count;
+        }
+
+        
+    }
+}
